7b89605ada946ba2740fcd089fed21c542bdf27d
ZhihuaSun <sunzhi@udel.edu>
2025-04-10 17:44:12 -0400

    new TimeManager

---
+++ assets/TimerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public float timeLeft = 60.0f;
+    
+    public Text timerText;
+    
+    public Text failText;
+
+    void Start()
+    {
+        if (failText != null)
+        {
+            failText.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            
+            float displayTime = Mathf.Max(timeLeft, 0);
+            
+            if (timerText != null)
+            {
+                timerText.text = Mathf.Ceil(displayTime).ToString();
+            }
+        }
+        else
+        {
+            if (timerText != null)
+            {
+                timerText.text = "0";
+            }
+            
+            if (failText != null && !failText.gameObject.activeSelf)
+            {
+                failText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
